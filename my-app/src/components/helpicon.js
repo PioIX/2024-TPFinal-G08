36956779b05e757b2bdf7d0d6693932785f74cf9
ailinhardt <164355430+sofiacariglino@users.noreply.github.com>
2024-10-14 08:41:46 -0300

    .

--- my-app/src/components/helpicon.js
+++ my-app/src/components/helpicon.js
@@ -5,11 +5,14 @@ const HelpIcon = () => {
   return (
     <div className="fixed top-5 right-5 mt-2">
       <div className="w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center cursor-pointer hover:bg-gray-300">
-        <span className="text-xl text-gray-500">Ayuda</span>
+        <button className="btn btn-success btn-lg btn-block" type="submit" onClick={ayuda}>Ayuda</button>
       </div>
     </div>
+    
   );
 };
 
 export default HelpIcon;
+
+/***span className="text-xl text-gray-500" */
 
