e47030362846b9161065e72a368a0575cbd1ad45
tomasmaraval <164355430+sofiacariglino@users.noreply.github.com>
2024-10-16 13:04:03 -0300

    pagina votaciones falta terminar

---
+++ my-app/src/app/menu/votar/page.js
@@ -0,0 +1,67 @@
+"use client";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import HelpIcon from '@/components/helpicon';
+
+export default function Votacion() {
+  return (
+    <>       
+      <HelpIcon></HelpIcon>
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
+      <section className="vh-100" style={{ backgroundColor: '#fbfcf7' }}>
+        <div className="card-body p-5 text-center">
+          <h2 className="mb-4" style={{ color: '#c87c8d', fontSize: '2rem', marginTop: '2%', fontFamily: 'Lora, serif' }}>Top 5 más votados</h2>
+        </div>
+
+        <div className="text-center" style={{ marginTop: '-10px', height: 'auto' }}>
+        <div style={{ 
+                width: '90%',  
+                height: '70vh',  // Ajusta el tamaño para que no ocupe toda la pantalla
+                backgroundColor: '#efe8e5', 
+                borderRadius: '10px', 
+                margin: '0 auto',
+                padding: '20px'
+                }}>
+            <div style={{ flex: 1, textAlign: 'center' }}>
+              <h3 style={{ color: '#c87c8d', fontFamily: 'Lora, serif', fontSize: '1.75rem' }}>Outfits</h3>
+
+              <div style={{ 
+                fontFamily: 'Lora, serif', 
+                fontSize: '1.5rem', 
+                color: '#bf97a0',  
+                textAlign: 'center', 
+                display: 'flex', 
+                justifyContent: 'center',  
+                gap: '40px',  
+                marginTop: '20px', 
+                marginBottom: '20px' 
+              }}>
+                {Array.from({ length: 5 }, (_, index) => (
+                  <div key={index} style={{ flex: 1, textAlign: 'center' }}>
+                    <h3 style={{ color: '#bfb8b8' }}>Jugador</h3>
+                    <div style={{ 
+                      width: '100%',  
+                      height: '80%', 
+                      backgroundColor: '#fff6f2', 
+                      borderRadius: '10px', 
+                      margin: '0 auto',
+                      padding: '40px' 
+                    }}></div>
+                    <h5 style={{ color: '#bfb8b8' }}>Puntaje</h5>
+                    <div style={{ marginTop: '10px', color: '#bfb8b8' }}>
+                      1 2 3 4 5
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
