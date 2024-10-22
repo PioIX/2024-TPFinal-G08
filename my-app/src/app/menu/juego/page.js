c466d3a180bf17281f92a59f4ef961a208ee6878
ailinhardt <162989571+ailinhardt@users.noreply.github.com>
2024-10-22 08:23:18 -0300

    ultimos cambios (creo)

--- my-app/src/app/menu/juego/page.js
+++ my-app/src/app/menu/juego/page.js
@@ -37,32 +37,35 @@ export default function Game() {
       <HelpIcon />
 
       <section style={{ backgroundColor: '#fbfcf7', minHeight: '100vh', padding: '20px' }}>
-        {/* Div de fondo que cubre todos los elementos */}
         <div style={{ 
-          backgroundColor: '#fbf3f3', /* Rosita pastel más fuerte para el fondo */
+          backgroundColor: '#fbf3f3', 
           padding: '20px', 
           borderRadius: '10px', 
-          width: '80%',  /* Achicado el fondo */
+          width: '80%', 
           margin: '0 auto', 
           zIndex: 1 
         }}>
-          <div className="game-container" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <div className="game-container" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
             
-            {/* Panel del avatar */}
             <div className="avatar-panel" style={{ width: '40%', textAlign: 'center', padding: '20px' }}>
-              <div style={{ backgroundColor: '#ffffff', padding: '15px', borderRadius: '10px', minHeight: '300px' }}>
+              <div style={{ backgroundColor: '#ffffff', padding: '15px', borderRadius: '10px', minHeight: '350px' }}>
+                <div style={{ 
+                  backgroundColor: '#bf97a0', 
+                  color: 'white', 
+                  borderRadius: '10px', 
+                  padding: '3px 5px', 
+                  marginBottom: '8px' 
+                }}>
+                  Usuario
+                </div>
                 <div className="avatar" style={{ padding: '20px', borderRadius: '10px' }}>
                   <img src="/avatar.png" alt="Avatar" style={{ width: '80%' }} />
                 </div>
               </div>
             </div>
 
-            {/* Panel de selección de ropa */}
-            <div className="clothing-panel" style={{ width: '55%', textAlign: 'center', padding: '20px', backgroundColor: "#efe8e5", borderRadius: '10px'  }}>
-
-              {/* Nuevo contenedor para los botones y el selector de prendas */}
+            <div className="clothing-panel" style={{ width: '55%', textAlign: 'center', padding: '20px', backgroundColor: "#efe8e5", borderRadius: '10px' }}>
               <div className="buttons-and-selector-container" style={{ marginBottom: '20px' }}>
-                {/* Contenedor solo para los botones */}
                 <div className="category-selector" style={{ display: 'flex', justifyContent: 'space-around', marginBottom: '10px' }}>
                   {categories.map(({ name, img }) => (
                     <button 
@@ -74,9 +77,8 @@ export default function Game() {
                         backgroundColor: selectedCategory === name ?  '#bf97a0' : '#fbf3f3',
                         borderRadius: '5px',
                       }}>
-                      {/* Imagen en lugar de texto dentro del botón */}
                       <img 
-                        src={`/${img}`} // Ruta de la imagen que usarás para los botones
+                        src={`/${img}`} 
                         alt={name} 
                         style={{ width: '40px', height: '40px' }} 
                       />
@@ -84,21 +86,44 @@ export default function Game() {
                   ))}
                 </div>
 
-                {/* Selector de prenda */}
                 <div className="clothing-items" style={{ backgroundColor: '#fff6f2', padding: '20px', borderRadius: '10px', minHeight: '300px' }}>
                   <img src={`/${selectedCategory}.png`} alt={selectedCategory} style={{ width: '60px', margin: '10px' }} />
                 </div>
 
-              </div> {/* Cierre del nuevo div buttons-and-selector-container */}
+              </div>
             </div>
           </div>
+
+          {/* Botones de acción debajo del div del avatar */}
+          <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '20px' }}>
+            <button style={{
+              padding: '10px 20px',
+              border: 'none',
+              backgroundColor: '#bf97a0',
+              color: 'white',
+              borderRadius: '20px', // Más redondeado
+              cursor: 'pointer',
+            }}>
+              Guardar Outfit
+            </button>
+            <button style={{
+              padding: '10px 20px',
+              border: 'none',
+              backgroundColor: '#bf97a0',
+              color: 'white',
+              borderRadius: '20px', // Más redondeado
+              cursor: 'pointer',
+            }}>
+              Salir sin Guardar
+            </button>
+          </div>
+
         </div>
       </section>
 
-      {/* Estilos globales para el body */}
       <style jsx global>{`
         body {
-          background-color: #fff; /* Cambia el color según lo que necesites */
+          background-color: #fbfcf7;
         }
       `}</style>
     </>
