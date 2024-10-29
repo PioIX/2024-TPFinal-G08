5497ca212c2f5fed10f72732b782638bf8f2b013
IgnacioGabrielRuiz <162989282+anabone@users.noreply.github.com>
2024-10-29 08:39:41 -0300

    IMAGENES

--- my-app/src/app/menu/juego/page.js
+++ my-app/src/app/menu/juego/page.js
@@ -5,8 +5,6 @@ import React from 'react'
 import MuestraRopa from '@/components/MuestraRopa'
 
 export default async function Juego(){
-    const array = [1, 2, 3, 4, 5, 6]
-
     return(
         <>
             <MuestraRopa></MuestraRopa>
--- my-app/src/components/MuestraRopa.js
+++ my-app/src/components/MuestraRopa.js
@@ -6,6 +6,8 @@ import styles from '@/components/page.module.css';
 
 export default function MuestraRopa() {
     const [remeras, setRemeras] = useState([]);
+    const [remeraSeleccionada, setRemeraSeleccionada] = useState("");
+
 
     async function obtenerRemeras() {
         let res = await getRemeras();
@@ -21,13 +23,24 @@ export default function MuestraRopa() {
     }, [remeras]);
 
     let outfit = {
-        remeras: 0, 
+        remeras: 0,
         personaje: 1
     }
 
     function ids(id) {
         outfit.remeras = id
-        console.log(outfit); // Aquí imprimimos el id
+        console.log(outfit);
+        let newRemera = "";
+        console.log(remeras.idRemeras)
+        console.log(outfit.remeras)
+        for (let i = 0; i < remeras.length; i++) {
+            if (outfit.remeras == remeras[i].idRemeras) {
+                newRemera = remeras[i].link
+            }
+        }
+        console.log(newRemera)
+        setRemeraSeleccionada(newRemera);
+        console.log(remeraSeleccionada)
     }
 
     return (
@@ -35,10 +48,12 @@ export default function MuestraRopa() {
             <div className={styles.ropa}>
                 <div className={styles.MuestraOutfit}>
                     <div className={styles.MuestraPersonaje}>
-                        <img src="/personajes/nano.png" style={{ width: '270px', height: '480px' }}/>
+                        <img src="/personajes/nano.png" style={{ width: '270px', height: '480px' }} />
                     </div>
                     <div id="remera" className={styles.MuestraRemera}>
-                        <img src='/ropa/remeras/remera azul.png' style={{ width: '270px', height: '480px' }}/>
+                        {remeraSeleccionada && (
+                            <img src={remeraSeleccionada} style={{ width: '270px', height: '480px' }} alt="Remera Seleccionada" />
+                        )}
                     </div>
                 </div>
                 <div className={styles.MuestraRopa}>
@@ -66,4 +81,8 @@ export default function MuestraRopa() {
             })
             }
             <Button onClick={obtenerRemeras} text="HOLAAAA"></Button>
+
+
+
+            <img src='/ropa/remeras/remera violeta.png' style={{ width: '270px', height: '480px' }} />
             */
