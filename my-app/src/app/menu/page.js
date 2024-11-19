048e5995adda96927cd746dd4d0e40a6e0ac3294
feli9 <162989571+ailinhardt@users.noreply.github.com>
2024-11-19 08:43:46 -0300

    a

--- my-app/src/app/menu/page.js
+++ my-app/src/app/menu/page.js
@@ -4,10 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Link from 'next/link';
 import HelpIcon from '@/components/helpicon';
-import { getRemeras, getPantalones, getPersonajes, getCalzados, getAccesorios, getMascotas, getOutfits } from '@/app/utils/api.js'
+import { getRemeras, getPantalones, getPersonajes, getCalzados, getAccesorios, getMascotas, getOutfits } from '@/app/utils/api.js';
 import Hamburguesa from '@/components/Hamburguesa';
 
-
+// cada categoría tiene un estado que almacena su lista de opciones y el elemento seleccionado
+// `selectedCategory` maneja la categoría activa en la interfaz
 export default function pagina() {
   const handleJugarClick = () => {
     window.location.href = '/menu/juego'; // Redirige a /juego
@@ -25,8 +26,6 @@ export default function pagina() {
     window.location.href = '/menu/votar'; // Redirige a /votar
   };
 
-  
-
   return (
     <>
       <head>
@@ -35,12 +34,15 @@ export default function pagina() {
       </head>
       <section className="vh-100" style={{ backgroundColor: '#fbfcf7' }}>
         <div className="card-body p-5 text-center">
+          {/* Panel del Avatar */}
           <h2 className="mb-4" style={{ color: '#d8bfc5', fontSize: '3rem', marginTop: '10%', fontFamily: 'Lora, serif' }}>
             Dress To Impress!
           </h2>
           <div style={{ marginBottom: '20px' }}>
             <img src="/ribbon.png" alt="Descripción de la imagen" className="img-fluid" style={{ height: '120px' }} />
           </div>
+
+          {/* Botones de acción debajo del div del avatar */}
           <button 
             className="btn btn-success btn-lg btn-block" 
             type="button" 
